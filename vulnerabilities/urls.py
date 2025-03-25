ece07fececd1fdeb93d0808d31dcc52def5f1957
Steve Kueng <steve.kueng@umb.ch>
2025-03-25 13:03:05 +0100

    fix path warning

--- vulnerabilities/urls.py
+++ vulnerabilities/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.vulnerabilities_overview, name="vulnerabilities"),
-    path("_json$", views.vulnerabilities_api_overview, name="vulnerabilities_api_overview"),
+    path("_json", views.vulnerabilities_api_overview, name="vulnerabilities_api_overview"),
 ]
